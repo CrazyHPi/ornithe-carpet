91eb5d29fdecd52c0bc97795a493bb6bc76d2f51
void <skeletonvoid0@gmail.com>
2024-08-13 23:03:12 +0800

    - Make MixinExtras actually properly initialize

---
+++ src/main/java/carpet/bootstrap/mixinextras/MixinExtrasInitPlugin.java
@@ -0,0 +1,44 @@
+package carpet.bootstrap.mixinextras;
+
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class MixinExtrasInitPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+        MixinExtrasBootstrap.init();
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
